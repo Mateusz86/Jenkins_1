32e5471eef823fe6ed75a63f9f4572c9d94a2908
Mateusz Drożdż <drozdz@nonstop.co>
2019-08-07 14:09:58 +0200

    first test

--- src/test/java/com/drozdz/jnkins/JnkinsApplicationTests.java
+++ src/test/java/com/drozdz/jnkins/JnkinsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.drozdz.jnkins;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,8 @@ public class JnkinsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		assertTrue(Boolean.TRUE);
 	}
 
 }
