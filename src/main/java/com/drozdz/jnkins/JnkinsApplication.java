74ced01d59f7c3b1542c3b979dbf2c766ff4ed63
Mateusz Drożdż <drozdz@nonstop.co>
2019-08-07 13:59:26 +0200

    first commit

---
+++ src/main/java/com/drozdz/jnkins/JnkinsApplication.java
@@ -0,0 +1,13 @@
+package com.drozdz.jnkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnkinsApplication.class, args);
+	}
+
+}
